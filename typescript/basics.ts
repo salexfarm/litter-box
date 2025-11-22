8cc33f260d045b2c9951533970c992715a99a442
Stephen Farmer <safarmer@icloud.com>
2025-11-22 19:51:00 +0000

    Adding Udemy tutorial file

---
+++ typescript/basics.ts
@@ -0,0 +1,19 @@
+// 11. Using Node.js To Run JavaScript Code
+
+/**
+ * In this and many other sections, I'll occasionally run compiled code with Node.js - simply to save us the hassle of
+ *
+ * building a (demo) website that loads and executes that code.
+ *
+ * You can install the Node.js runtime from https://nodejs.org/en (it's available for all operating systems).
+ *
+ * This runtime allows you to run JavaScript code right on your system (via the node command) instead of requiring a
+ *
+ * website. So when you see me run node app.js (or some other file), that's me using Node.js to execute the code.
+ **/
+
+// 12. Project Setup
+
+console.log('Hello World!');
+console.log("Hello World!");
+
